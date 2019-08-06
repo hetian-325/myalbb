8179590ab8caa9f86fba9525613313077a6f97d5
hetian <544205481@qq.com>
2019-08-06 21:05:22 +0800

    改了首页

--- js/index.js
+++ js/index.js
@@ -1,27 +1,30 @@
-// 轮播图
+	// 轮播图
 	let box1 = $(".ban_cnt_top");
 	let box2 = $(".bct_box_1");
 	let box3 = $(".bct_box_2");
 	let arrImg1 = ['url(img/banner1.webp)','url(img/banner4.webp)','url(img/banner7.webp)','url(img/banner10.webp)','url(img/banner13.webp)'];
 	let arrImg2 = ['url(img/banner2.webp)','url(img/banner5.webp)','url(img/banner8.webp)','url(img/banner11.webp)','url(img/banner14.webp)'];
 	let arrImg3 = ['url(img/banner3.webp)','url(img/banner6.webp)','url(img/banner9.webp)','url(img/banner12.webp)','url(img/banner15.webp)'];
-
+	let liDoms = $(".douBox")[0].children;
 	let myTimer = null;
 	let ord = 0;
-	$(function(){
-		box1.css("background",arrImg1[ord]);
-		reset();
-		autoplay();
+	function autoPlay(){
+		if(myTimer!=null){
+			return;
+		}
 		myTimer = setInterval(function(){
+			var preOrd = ord;
 			reset();
 			ord++;
 			if(ord>4){
 				ord=0;
 			}
-			autoplay();
+			auto();
+			liDoms[preOrd].style.backgroundColor="";
+			liDoms[ord].style.backgroundColor="red";
 		},3000);
-	})
-	function autoplay(){
+	}
+	function auto(){
 		box1.css("background",arrImg1[ord]);
 		box2.css("background",arrImg2[ord]);
 		box3.css("background",arrImg3[ord]);
@@ -42,9 +45,78 @@
 			left:'-50px'
 		});
 	}
+	function stopPlay(){
+		window.clearInterval(myTimer);
+		myTimer = null;
+	}
+	function goImg(transOrd){
+		var preOrd = ord;
+		ord = transOrd;
 
+		if(ord>4){
+			ord = 0;
+		}else if(ord<0){
+			ord = 4;
+		}
+		render(preOrd,ord);
+	}
+	function render(preOrd,ord){
+		reset();
+		auto();
+		// 改豆豆的颜色
+		liDoms[preOrd].style.backgroundColor="";
+		liDoms[ord].style.backgroundColor="red";
+	}
+	function preImg(){
+		goImg(ord-1);
+	}
+	function nextImg(){
+		goImg(ord+1);
+	}
 
 window.onload = function(){
+	let liDoms = $(".douBox")[0].children;
+	liDoms[ord].style.backgroundColor="red";
+	box1.css("background",arrImg1[ord]);
+	reset();
+	auto();
+	//自动播放
+	autoPlay();
+	//鼠标放在轮播图上会停止
+	$(".ban_cnt_top")[0].onmouseover = function(){
+		stopPlay();
+		$(".arrowBox > span").css("display","block");
+	};
+	//鼠标离开轮播图会继续播放
+	$(".ban_cnt_top")[0].onmouseout = function(){
+		autoPlay();
+		$(".arrowBox > span").css("display","none");
+	};
+	//鼠标移入按钮
+	$(".arrowBox > span").mouseenter(function(){
+		$(this).css("opacity",".6");
+	});
+	//鼠标移出按钮
+	$(".arrowBox > span").mouseleave(function(){
+		$(this).css("opacity",".3");
+	});
+	//点击豆豆，跳转到对应的图片
+	for(var i=0;i<liDoms.length;i++){
+		liDoms[i].setAttribute("index",i);
+		liDoms[i].onclick = function(){
+			goImg(parseInt(this.getAttribute("index")));
+		};
+	}
+	//左右按钮
+	let leftBtn = $(".arrowBox")[0].firstElementChild;
+	leftBtn.onclick = function(){
+		preImg();
+	}
+	let rightBtn = $(".arrowBox")[0].lastElementChild;
+	rightBtn.onclick = function(){
+		nextImg();
+	}
+	// 轮播图结束
 	// 顶部左侧
 	$(".cnt_c")[0].onmouseover = function(){
 		$(".c_box")[0].style.display = "block";
@@ -163,9 +235,17 @@ window.onload = function(){
 
 $(function(){
 	$(".ban_cnt_btm > span:first").click(function(){
-		$(".bcb_box").animate({left:'+=700px'},"slow");
+		if($(".bcb_bigBox").css('left')>="0px"){
+			$(".bcb_bigBox").animate({left:'0px'});
+		}else{
+			$(".bcb_bigBox").animate({left:'+=700px'},"slow");
+		}
 	});
 	$(".ban_cnt_btm > span:last").click(function(){
-		$(".bcb_box").animate({left:'-=700px'},"slow");
+		if($(".bcb_bigBox").css('left')<="-1400px"){
+			$(".bcb_bigBox").animate({left:'-1400px'});
+		}else{
+			$(".bcb_bigBox").animate({left:'-=700px'},"slow");
+		}
 	});
 });
